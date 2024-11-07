8c8e412fbb92f9bededfa5084c738eb9c46ec4f4
marjourie123 <margopacturan@gmail.com>
2024-11-07 00:54:57 -0800

    Add files via upload

---
+++ margo/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		Ubuntu Server PHP Deployment
+	</title>
+</head>
+<link href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">" crossorigin="anonymous">
+<body>
+	<p>Ubuntu Server PHP Deployment- Marjourie Pacturan<p>
+
+<div class="container">
+	<label>Firstname:</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+<div class="container">
+	<label>Middle Name:</label>
+    <input type="text" name="middlename" class="form-control">
+</div>
+<div class="container">
+	<label>Last Name:</label>
+    <input type="text" name="lastname" class="form-control">
+</div>
+<div class="container">
+	<label>Age:</label>
+    <input type="text" name="age" class="form-control">
+</div>
+<div class="container">
+	<label>Address:</label>
+    <input type="text" name="address" class="form-control">
+</div>
+<div class="container">
+	<label>Course&Section:</label>
+    <input type="text" name="course&section" class="form-control">
+
+    <button type="submit">Submit</button>
+</div>
+</body>
+</html>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
